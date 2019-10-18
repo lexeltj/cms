95918402130ae3563c6c90d5f957d62a9ea81633
lexel <lexel.tj@googlemail.com>
2019-10-18 10:57:56 +0200

    extract

--- src/Posts/PostsController.php
+++ src/Posts/PostsController.php
@@ -12,12 +12,12 @@ class PostsController {
     
     protected function render($view, $params) {
         
-        foreach ($params as $key => $value) {
-            ${$key} = $value;
-        }
+       //foreach ($params as $key => $value) {
+       //     ${$key} = $value;
+       // }
         
  
-       
+       extract($params);
        
         include __DIR__ . "/../../views/{$view}.php";
     }
